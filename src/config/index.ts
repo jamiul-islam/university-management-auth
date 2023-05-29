f9d94fd167729a7fde75d06c42603b56fe6f6fd6
jamiul-islam <jamiulislamjami@hotmail.com>
2023-05-29 16:09:54 +0600

    first commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
   port: process.env.PORT,
   databaseUrl: process.env.DATABASE_URL,
-  env: process.env.NODE_ENV || "development",
-};
+  env: process.env.NODE_ENV || 'development',
+}
