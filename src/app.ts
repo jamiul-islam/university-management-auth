8d17b90b44525fe7afe5d29d46a1b7dc85a86139
jamiul-islam <jamiulislamjami@hotmail.com>
2023-05-29 12:20:38 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app: Application = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// testing route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to my API");
 });
 
 export default app;
